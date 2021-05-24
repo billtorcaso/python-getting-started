a02584b2a7dd904ffa2da69da736e2bd51078a02
Bill Torcaso <torcasobill@gmail.com>
2021-05-24 14:38:06 -0400

    An experiment: add a URL for "/hello", in addition to the empty path "" resolving to the index file.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,7 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
+    path("hello", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
