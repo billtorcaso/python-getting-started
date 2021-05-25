9d9307a170059b91cabcefd9e629cef5711e5232
Bill Torcaso <torcasobill@gmail.com>
2021-05-25 09:29:12 -0400

    Fix typos in ifp and tfc views.py.

--- ifp/views.py
+++ ifp/views.py
@@ -4,11 +4,11 @@ from django.template.loader import render_to_string
 from django.shortcuts import render,HttpResponse
 
 def index(request):
-    return HttpResponse("<h1> Second Deploy:Innovators For Purpose </h1>"
-                        "<p> This page is ifp, method is 'index' </p>")
-                        "<p> This codebase is GIT branch 'bt-2nd-deploy' </p>")
-                        "<p> This dyno is named 'bt-2nd-deploy.herokuapp.com' </p>")
-                        "<br/><p> Hello, world! </p>")
+    return HttpResponse("""<h1> Second Deploy:Innovators For Purpose </h1>
+                        <p> This page is /ifp, method is 'index' </p>
+                        <p> This codebase is GIT branch 'bt-2nd-deploy' </p>
+                        <p> This dyno is named 'bt-2nd-deploy.herokuapp.com' </p>
+                        <br/><p> Hello, world! </p>""")
 
 def about(request):
     result = """<h1> This is the 'About' page </h1>
--- tools_for_change/views.py
+++ tools_for_change/views.py
@@ -4,17 +4,17 @@ from django.template.loader import render_to_string
 from django.shortcuts import render,HttpResponse
 
 def index(request):
-    return HttpResponse("<h1> Second Deploy: Tools For Curiousity </h1>"
-                        "<p> This page is tfc, method is 'index' </p>")
-                        "<p> This codebase is GIT branch 'bt-2nd-deploy' </p>")
-                        "<p> This dyno is named 'bt-2nd-deploy.herokuapp.com' </p>")
-                        "<br/><p> Hello, world! </p>")
+    return HttpResponse("""<h1> Second Deploy: Tools For Curiousity </h1>
+                        <p> This page is tfc, method is 'index' </p>
+                        <p> This codebase is GIT branch 'bt-2nd-deploy' </p>
+                        <p> This dyno is named 'bt-2nd-deploy.herokuapp.com' </p>
+                        <br/><p> Hello, world! </p>""")
 
 def about(request):
-    return HttpResponse("<h2> Second Deploy: Tools For Curiousity </h2>"
-                        "<p> Hello, world! </p>"
-                        "<br/>"
-                        "<p> Tools For Curiousity is a project "
-                        "to create a cellphone-playable video game."
-                        "The game will include puzzles, levels, "
-                        "and a 'Save' feature.")
+    return HttpResponse("""<h2> Second Deploy: Tools For Curiousity </h2>
+                        <p> Hello, world! </p>
+                        <br/>
+                        <p> Tools For Curiousity is a project 
+                        to create a cellphone-playable video game.
+                        The game will include puzzles, levels, 
+                        and a 'Save' feature.""")
